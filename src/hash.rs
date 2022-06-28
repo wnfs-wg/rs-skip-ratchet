0e6f466433e9e4df6df85928656e7637c5305b95
Stephen Akinyemi <appcypher@outlook.com>
2022-06-29 00:00:36 +0100

    Fix test

--- src/hash.rs
+++ src/hash.rs
@@ -103,18 +103,15 @@ mod hash_tests {
     use super::*;
 
     #[test]
-    fn from_item_hashes() {
-        let seed = Hash::from_raw([0xFF; 32]);
+    fn can_hash_items() {
+        let hash = Hash::from(&b"An example of a byte array");
+        let expected = Hash::from_raw([
+            0xa1, 0x6f, 0x2d, 0x12, 0x23, 0x21, 0x11, 0xb2, 0xb8, 0xca, 0x57, 0x02, 0xcf, 0x55,
+            0x25, 0x57, 0xfb, 0xff, 0xc3, 0x40, 0x22, 0x72, 0x62, 0x8e, 0x9c, 0xc0, 0x08, 0x89,
+            0x99, 0xd2, 0x4b, 0x2b,
+        ]);
 
-        println!("seed = {:?}", seed);
-
-        let hash = Hash::from(&!seed);
-
-        println!("hash = {:?}", hash);
-
-        let hash = Hash::from(&!seed);
-
-        println!("hash = {:?}", hash);
+        assert_eq!(hash, expected);
     }
 
     #[test]
@@ -149,7 +146,7 @@ mod hash_tests {
     #[test]
     fn from_n_hashes_n_times() {
         let hash_1 = {
-            let h = Hash::from(b"James");
+            let h = Hash::from(b"James Cameron");
             let h = Hash::from(&h);
             let h = Hash::from(&h);
             let h = Hash::from(&h);
@@ -157,7 +154,7 @@ mod hash_tests {
             h
         };
 
-        let hash_2 = Hash::from_chain(b"James", 5);
+        let hash_2 = Hash::from_chain(b"James Cameron", 5);
 
         assert_eq!(hash_1, hash_2);
     }
